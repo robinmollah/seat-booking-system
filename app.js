0c09871f90e251a97db6b5aeeddcaeb840795c77
robinmollah <robinsajin@gmail.com>
2021-07-25 03:41:18 +0600

    Login form

---
+++ app.js
@@ -0,0 +1,21 @@
+if(process.env.NODE_ENV !== 'production'){
+	require('dotenv').config()
+}
+const port = process.env.PORT | 3002;
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.render("index", {title: "Dashboard"})
+})
+
+
+app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`);
+	console.log('Version: 0.1.2');
+})
+
